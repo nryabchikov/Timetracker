d87f07c199fc950fcade3623c4afaff3d15ba937
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-14 10:25:24 +0300

    feat: user service(business logic + abstraction of user repository)

---
+++ src/main/java/by/krainet/timetracker/service/UserService.java
@@ -0,0 +1,45 @@
+package by.krainet.timetracker.service;
+
+import by.krainet.timetracker.exception.UserNotFoundException;
+import by.krainet.timetracker.model.User;
+import by.krainet.timetracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(updatedUser.getUsername());
+            user.setPassword(updatedUser.getPassword());
+            user.setEmail(updatedUser.getEmail());
+            user.setRole(updatedUser.getRole());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    public void deleteUser(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+        } else {
+            throw new UserNotFoundException(id);
+        }
+    }
+}
