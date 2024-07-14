8e8bd89c0dac72e802ba5ded802b8a98f8dfa89b
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-14 10:22:42 +0300

    feat: model project

---
+++ src/main/java/by/krainet/timetracker/model/Project.java
@@ -0,0 +1,22 @@
+package by.krainet.timetracker.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "projects")
+@Data
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Task> tasks;
+}
